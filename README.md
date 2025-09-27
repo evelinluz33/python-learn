88b859f6ea732856bb83f645913f89d973b9bfd3
waldoaraque <waldoaraque10@gmail.com>
2025-09-27 20:58:00 +0200

    haciendo el codigo orientado a objetos (POO)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # python-learn
 proyecto para aprender python
+
+
+### Calculadora
+
+```calculadora/main.py```
+Contiene el codigo para hacer las operaciones basicas de una calculadora.
+
--- calculadora/main.py
+++ calculadora/main.py
@@ -1,39 +1,46 @@
 
 
+class Calculadora:
 
-def suma(a: int, b: int) -> int:
-    return a + b
+    def __init__(self, a: int, b: int, operacion: str) -> None:
+        self.a = a
+        self.b = b
+        self.op = operacion
+    
+    def suma(self) -> int:
+        return self.a + self.b
 
-def resta(a: int, b: int) -> int:
-    return a - b
+    def resta(self) -> int:
+        return self.a - self.b
 
-def multiplicacion(a: int, b: int) -> int:
-    return a * b
+    def multiplicacion(self) -> int:
+        return self.a * self.b
 
-def division(a: int, b: int) -> float:
-    if b == 0:
-        raise ValueError("No se puede dividir por cero.")
-    return a / b
+    def division(self) -> float:
+        if b == 0:
+            raise ValueError("No se puede dividir por cero.")
+        return self.a / self.b
+    
+    def resultado(self, resultado) -> None:
+        print(f"{self.a} {self.op} {self.b} = {resultado}")
 
-def procesar(funcion):
+
+def procesar():
+    input("Presione Enter para iniciar la calculadora...")
+    operacion = input("Escribe operación (+, -, *, /): ")
     numero_a = input("Digite o primer número: ")
     numero_b = input("Digite o segundo número: ")
-    print(f"{numero_a} + {numero_b} = {funcion(int(numero_a), int(numero_b))}")
+    calculadora = Calculadora(int(numero_a), int(numero_b), operacion)
+    if operacion == '+':
+        calculadora.resultado(calculadora.suma())
+    elif operacion == '-':
+        calculadora.resultado(calculadora.resta())
+    elif operacion == '*':
+        calculadora.resultado(calculadora.multiplicacion())
+    elif operacion == '/':
+        calculadora.resultado(calculadora.division())
+    else:
+        raise ValueError("No es una operación válida.")
+        exit(1)
 
-input("Presione Enter para iniciar la calculadora...")
-
-operacion = input("Escribe operación (+, -, *, /): ")
-
-#if operacion not in ('+', '-', '*', '/'):
-
-if operacion == '+':
-    procesar(suma)
-elif operacion == '-':
-    procesar(resta)
-elif operacion == '*':
-    procesar(multiplicacion)
-elif operacion == '/':
-    procesar(division)
-else:
-    raise ValueError("No es una operación válida.")
-    exit(1)
+procesar()
